9863f266e57377d2783f276a924b706a19312968
Jason-19 <jasonnunez02016@gmail.com>
2025-05-03 22:59:51 -0500

    Quit Token Required in EndPoint User

--- controller/UserController.py
+++ controller/UserController.py
@@ -6,41 +6,57 @@ from sqlalchemy.orm import Session
 class UsersController:
     # @staticmethod
     def get_user(db:Session):
-        user= db.query(User).all()
-        if not user:
-            return jsonify({"error": "There are no users is empty"}), 404
-        return jsonify([user.serialize() for user in user]), 200
+        try:
+            user= db.query(User).all()
+            if not user:
+                return jsonify({"error": "There are no users is empty"}), 404
+            return jsonify([user.serialize() for user in user]), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
     
     
     def getUserById(db:Session,user_id):
-        user= db.query(User).where(User.user_id == user_id).first()
         
-        if not user:
-            return jsonify({"error": "User not found"}), 404
-        return jsonify(user.serialize()), 200
+        try:
+            
+            user= db.query(User).where(User.user_id == user_id).first()
+            
+            if not user:
+                return jsonify({"error": "User not found"}), 404
+            return jsonify(user.serialize()), 200
+        
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
     
     def getUserLevel(data):
         
-        data = request.json
+        try:
+            data = request.json
         
-        if not data:
-            return jsonify({'error': 'No data provided'}), 400
+            if not data:
+                return jsonify({'error': 'No data provided'}), 400
+            
+            if not data["user_id"]:
+                return jsonify({'error': 'No user id provided'}), 400
+            
+            print("recibi datos")
+            user_id = data["user_id"]  
+            
+            user = g.db.query(User).filter_by(user_id=user_id).first()
+            print("encontre usuario")
+            if not user:
+                return jsonify({"error": "User not found"}), 404
+            
+            user_level = user.challenge_level_id    
+            user_progress = user.challenge_progress
+            
+            response = {
+                "user_level": user_level,
+                "user_progress": user_progress
+            }
+            
+            return jsonify(response), 200
         
-        if not data["user_id"]:
-            return jsonify({'error': 'No user id provided'}), 400
-        print("recibi datos")
-        user_id = data["user_id"]  
-        user = g.db.query(User).filter_by(user_id=user_id).first()
-        print("encontre usuario")
-        if not user:
-            return jsonify({"error": "User not found"}), 404
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
         
-        user_level = user.challenge_level_id    
-        user_progress = user.challenge_progress
-        
-        response = {
-            "user_level": user_level,
-            "user_progress": user_progress
-        }
-        
-        return jsonify(response), 200
--- routes/Users.py
+++ routes/Users.py
@@ -7,12 +7,12 @@ user_route = Blueprint('users',__name__)
 ucontroller = UsersController
 
 @user_route.route("/users/all",methods=['GET'])
-@token_required
+# @token_required
 def getUsersEndpoint():
     return ucontroller.get_user(g.db)
 
 @user_route.route("/users/<user_id>/",methods=['GET'])
-@token_required
+# @token_required
 def getUserByIdEndpoint(user_id):
     return ucontroller.getUserById(g.db,user_id)
 
